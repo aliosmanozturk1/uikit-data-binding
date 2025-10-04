f364688749b26c83b6857488821e431e386a0f9c
Ali Osman Öztürk <github@aliosmanozturk.me>
2025-10-04 10:16:25 +0300

    Initial Commit

---
+++ DataBinding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataBinding
+//
+//  Created by Ali Osman Öztürk on 4.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
